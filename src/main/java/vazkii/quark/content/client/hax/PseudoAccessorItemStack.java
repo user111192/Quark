47dd5b410fd40bc7ac0477ab9158c4e9de855422
yrsegal@gmail.com <yrsegal@gmail.com>
2022-06-26 21:51:03 -0400

    document the hax

--- src/main/java/vazkii/quark/content/client/hax/PseudoAccessorItemStack.java
+++ src/main/java/vazkii/quark/content/client/hax/PseudoAccessorItemStack.java
@@ -9,7 +9,14 @@ import vazkii.quark.content.client.resources.AttributeSlot;
 import java.util.List;
 import java.util.Map;
 
-// This is extremely jank. Please do not use this as an example for anything.
+/**
+ * This is extremely jank. Please do not use this as an example for anything unless you ABSOLUTELY know what you're doing.
+ *
+ * This class is implemented on {@link net.minecraft.world.item.ItemStack} via {@link vazkii.quark.mixin.ItemStackMixin}.
+ * Therefore, a stack can be cast to this interface, and subsequently use the interface methods implemented in the mixin.
+ *
+ * Names are prefixed with {@code quark$} to avoid clashing with other mixed-in methods.
+ */
 public interface PseudoAccessorItemStack {
 
 	Map<AttributeSlot, Multimap<Attribute, AttributeModifier>> quark$getCapturedAttributes();
