f4cb2cee193b9ce74c89a54a00f8df7a2174b2ea
Vasco Lavos <vazkii@hotmail.com>
2022-07-02 22:27:02 +0100

    Update door template

--- src/main/resources/genscripts/door.py
+++ src/main/resources/genscripts/door.py
@@ -1,10 +1,14 @@
 from jsongen import *
 
 copy([
-	('block_model_door_top.json', 'assets/{modid}/models/block/{name}_door_top.json'),
-	('block_model_door_bottom.json', 'assets/{modid}/models/block/{name}_door_bottom.json'),
-	('block_model_door_top_hinge.json', 'assets/{modid}/models/block/{name}_door_top_hinge.json'),
-	('block_model_door_bottom_hinge.json', 'assets/{modid}/models/block/{name}_door_bottom_hinge.json'),
+	('block_model_door_bottom_left.json', 'assets/{modid}/models/block/{name}_door_bottom_left.json'),
+	('block_model_door_bottom_left_opem.json', 'assets/{modid}/models/block/{name}_door_bottom_left_opem.json'),
+	('block_model_door_bottom_right.json', 'assets/{modid}/models/block/{name}_door_bottom_right.json'),
+	('block_model_door_bottom_right_open.json', 'assets/{modid}/models/block/{name}_door_bottom_right_open.json'),
+	('block_model_door_top_left.json', 'assets/{modid}/models/block/{name}_door_top_left.json'),
+	('block_model_door_top_left_open.json', 'assets/{modid}/models/block/{name}_door_top_left_open.json'),
+	('block_model_door_top_right.json', 'assets/{modid}/models/block/{name}_door_top_right.json'),
+	('block_model_door_top_right_open.json', 'assets/{modid}/models/block/{name}_door_top_right_open.json'),
 
 	('block_item_door.json', 'assets/{modid}/models/item/{name}_door.json'),
 	('blockstate_door.json', 'assets/{modid}/blockstates/{name}_door.json'),
