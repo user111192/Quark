43e330567d68a4c8a2ded2516d3ceb596cdfb8bc
Vasco Lavos <vazkii@hotmail.com>
2022-07-03 18:14:28 +0100

    fix mixin and build

--- src/main/java/vazkii/quark/mixin/StructureTemplateMixin.java
+++ src/main/java/vazkii/quark/mixin/StructureTemplateMixin.java
@@ -1,10 +1,10 @@
 package vazkii.quark.mixin;
 
-import net.minecraft.world.level.ServerLevelAccessor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
+import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
 import vazkii.quark.base.handler.StructureBlockReplacementHandler;
@@ -12,7 +12,7 @@ import vazkii.quark.base.handler.StructureBlockReplacementHandler;
 @Mixin(StructureTemplate.class)
 public class StructureTemplateMixin {
 
-	@ModifyVariable(method = "placeInWorld(Lnet/minecraft/world/level/ServerLevelAccessor;Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructurePlaceSettings;Ljava/util/Random;I)Z",
+	@ModifyVariable(method = "placeInWorld(Lnet/minecraft/world/level/ServerLevelAccessor;Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructurePlaceSettings;Lnet/minecraft/util/RandomSource;I)Z",
 			at = @At(value = "FIELD", target = "Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructureTemplate$StructureBlockInfo;nbt:Lnet/minecraft/nbt/CompoundTag;", ordinal = 0),
 			index = 22)
 	private BlockState captureLocalBlockstate(BlockState state, ServerLevelAccessor accessor) {
