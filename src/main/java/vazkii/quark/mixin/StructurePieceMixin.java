7ae362bdc6754afe6cf52a978bb38952706c358a
Vasco Lavos <vazkii@hotmail.com>
2022-07-11 02:12:17 +0100

    Fix #3819

--- src/main/java/vazkii/quark/mixin/StructurePieceMixin.java
+++ src/main/java/vazkii/quark/mixin/StructurePieceMixin.java
@@ -15,18 +15,20 @@ public class StructurePieceMixin {
 
 	@ModifyVariable(
 			method = "createChest(Lnet/minecraft/world/level/ServerLevelAccessor;Lnet/minecraft/world/level/levelgen/structure/BoundingBox;Lnet/minecraft/util/RandomSource;Lnet/minecraft/core/BlockPos;Lnet/minecraft/resources/ResourceLocation;Lnet/minecraft/world/level/block/state/BlockState;)Z",
-			at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/ServerLevelAccessor;setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;I)Z"),
+			at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/ServerLevelAccessor;setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;I)Z", 
+			shift = At.Shift.BY, by = -2),
 			argsOnly = true
-	)
+			)
 	protected BlockState modifyBlockstateForChest(BlockState state, ServerLevelAccessor accessor) {
 		return StructureBlockReplacementHandler.getResultingBlockState(accessor, state);
 	}
 
 	@ModifyVariable(
 			method = "placeBlock(Lnet/minecraft/world/level/WorldGenLevel;Lnet/minecraft/world/level/block/state/BlockState;IIILnet/minecraft/world/level/levelgen/structure/BoundingBox;)V",
-			at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/WorldGenLevel;setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;I)Z"),
+			at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/WorldGenLevel;setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;I)Z", 
+			shift = At.Shift.BY, by = -2),
 			argsOnly = true
-	)
+			)
 	protected BlockState modifyBlockstate(BlockState state, WorldGenLevel level) {
 		return StructureBlockReplacementHandler.getResultingBlockState(level, state);
 	}
