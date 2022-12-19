475ba74d8548f3f66fecf792c7df94c9b5b4da7a
Vasco Lavos <vazkii@hotmail.com>
2022-12-19 06:01:39 +0000

    own lex on the marketplace of ideas

---
+++ src/main/java/vazkii/quark/mixin/client/ForgeGuiMixin.java
@@ -0,0 +1,18 @@
+package vazkii.quark.mixin.client;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+import net.minecraftforge.client.gui.overlay.ForgeGui;
+import vazkii.quark.content.client.module.ElytraIndicatorModule;
+
+@Mixin(ForgeGui.class)
+public class ForgeGuiMixin {
+
+	@ModifyConstant(method = "renderArmor", constant = @Constant(intValue = 20))
+	private static int renderArmor(int curr) {
+		return ElytraIndicatorModule.getArmorLimit(curr);
+	}
+	
+}
