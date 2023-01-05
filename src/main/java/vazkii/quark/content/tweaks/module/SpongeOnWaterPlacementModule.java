1d18cd406fb31cf8d8de076030b1b025f5c1cb00
Vasco Lavos <vazkii@hotmail.com>
2023-01-05 20:42:20 +0000

    Sponge on water placement using lilypad logic

---
+++ src/main/java/vazkii/quark/content/tweaks/module/SpongeOnWaterPlacementModule.java
@@ -0,0 +1,40 @@
+package vazkii.quark.content.tweaks.module;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import vazkii.quark.base.module.LoadModule;
+import vazkii.quark.base.module.ModuleCategory;
+import vazkii.quark.base.module.QuarkModule;
+
+@LoadModule(category = ModuleCategory.TWEAKS, hasSubscriptions = true)
+public class SpongeOnWaterPlacementModule extends QuarkModule {
+
+	@SubscribeEvent
+	public void onUse(PlayerInteractEvent.RightClickItem event) {
+		ItemStack stack = event.getItemStack();
+		if(stack.is(Items.SPONGE)) {
+			Player player = event.getEntity();
+			Level level = event.getLevel();
+			InteractionHand hand = event.getHand();
+			
+			BlockHitResult blockhitresult = Item.getPlayerPOVHitResult(level, player, ClipContext.Fluid.SOURCE_ONLY);
+			BlockHitResult blockhitresult1 = blockhitresult.withPosition(blockhitresult.getBlockPos());
+			InteractionResult result = Items.SPONGE.useOn(new UseOnContext(player, hand, blockhitresult1));
+			if(result != InteractionResult.PASS) {
+				event.setCanceled(true);
+				event.setCancellationResult(result);
+			}
+		}
+	}
+
+}
