4407f358ce16a8916b33f656f2bf3f49fdc63cc7
Vasco Lavos <vazkii@hotmail.com>
2023-01-05 19:24:40 +0000

    Slab reversion recipe ignores existing shapes

--- src/main/java/vazkii/quark/content/tweaks/recipe/SlabToBlockRecipe.java
+++ src/main/java/vazkii/quark/content/tweaks/recipe/SlabToBlockRecipe.java
@@ -1,11 +1,16 @@
 package vazkii.quark.content.tweaks.recipe;
 
+import java.util.Optional;
+
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.CraftingRecipe;
 import net.minecraft.world.item.crafting.CustomRecipe;
 import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.item.crafting.SimpleRecipeSerializer;
 import net.minecraft.world.level.Level;
 import vazkii.quark.content.tweaks.module.SlabsToBlocksModule;
@@ -13,14 +18,17 @@ import vazkii.quark.content.tweaks.module.SlabsToBlocksModule;
 public class SlabToBlockRecipe extends CustomRecipe {
 	
 	public static final SimpleRecipeSerializer<?> SERIALIZER = new SimpleRecipeSerializer<>(SlabToBlockRecipe::new);
+	private boolean locked = false;
 	
 	public SlabToBlockRecipe(ResourceLocation id) {
 		super(id);
 	}
 
-	// TODO check for other potential matching recipes before accepting
 	@Override
 	public boolean matches(CraftingContainer container, Level level) {
+		if(locked)
+			return false;
+		
 		Item target = null;
 		
 		for(int i = 0; i < container.getContainerSize(); i++) {
@@ -29,7 +37,7 @@ public class SlabToBlockRecipe extends CustomRecipe {
 				Item item = stack.getItem();
 				
 				if(target != null)
-					return item == target;
+					return item == target && checkForOtherRecipes(container, level);
 				
 				if(SlabsToBlocksModule.recipes.containsKey(item)) {
 					target = item;
@@ -38,6 +46,20 @@ public class SlabToBlockRecipe extends CustomRecipe {
 		}
 		
 		return false;
+	}
+	
+	// very much doubt multiple threads would ever touch this but JUST IN CASE
+	private synchronized boolean checkForOtherRecipes(CraftingContainer container, Level level) {
+		locked = true;
+		boolean ret = false;
+		MinecraftServer server = level.getServer();
+		if(server != null) {
+			Optional<CraftingRecipe> optional = server.getRecipeManager().getRecipeFor(RecipeType.CRAFTING, container, level);
+			ret = !optional.isPresent();
+		}
+		
+		locked = false;
+		return ret;
 	}
 
 	@Override
