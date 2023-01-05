531600613f652222bc51d8e4182b8b57bbf8da09
Vasco Lavos <vazkii@hotmail.com>
2023-01-05 20:51:32 +0000

    Fix sponge being placed in air

--- src/main/java/vazkii/quark/content/tweaks/module/SpongeOnWaterPlacementModule.java
+++ src/main/java/vazkii/quark/content/tweaks/module/SpongeOnWaterPlacementModule.java
@@ -1,5 +1,6 @@
 package vazkii.quark.content.tweaks.module;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
@@ -9,6 +10,7 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -28,11 +30,15 @@ public class SpongeOnWaterPlacementModule extends QuarkModule {
 			InteractionHand hand = event.getHand();
 			
 			BlockHitResult blockhitresult = Item.getPlayerPOVHitResult(level, player, ClipContext.Fluid.SOURCE_ONLY);
-			BlockHitResult blockhitresult1 = blockhitresult.withPosition(blockhitresult.getBlockPos());
-			InteractionResult result = Items.SPONGE.useOn(new UseOnContext(player, hand, blockhitresult1));
-			if(result != InteractionResult.PASS) {
-				event.setCanceled(true);
-				event.setCancellationResult(result);
+			BlockPos pos = blockhitresult.getBlockPos();
+			
+			if(level.getBlockState(pos).is(Blocks.WATER)) {
+				BlockHitResult blockhitresult1 = blockhitresult.withPosition(pos);
+				InteractionResult result = Items.SPONGE.useOn(new UseOnContext(player, hand, blockhitresult1));
+				if(result != InteractionResult.PASS) {
+					event.setCanceled(true);
+					event.setCancellationResult(result);
+				}
 			}
 		}
 	}
