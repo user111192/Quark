3a9bfcea807232b7f9fd566bb05f5553bb932dc2
Vasco Lavos <vazkii@hotmail.com>
2023-01-06 05:48:02 +0000

    forgor the mosaic

--- src/main/java/vazkii/quark/content/building/module/BambooBackportModule.java
+++ src/main/java/vazkii/quark/content/building/module/BambooBackportModule.java
@@ -1,6 +1,9 @@
 package vazkii.quark.content.building.module;
 
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MaterialColor;
+import vazkii.quark.base.block.QuarkBlock;
 import vazkii.quark.base.handler.WoodSetHandler;
 import vazkii.quark.base.handler.WoodSetHandler.WoodSet;
 import vazkii.quark.base.module.LoadModule;
@@ -15,6 +18,8 @@ public class BambooBackportModule extends QuarkModule {
 	@Override
 	public void register() {
 		woodSet = WoodSetHandler.addWoodSet(this, "bamboo", MaterialColor.COLOR_YELLOW, MaterialColor.COLOR_YELLOW, false, false);
+		
+		new QuarkBlock("bamboo_mosaic", this, CreativeModeTab.TAB_BUILDING_BLOCKS, BlockBehaviour.Properties.copy(woodSet.planks));
 	}
 	
 }
