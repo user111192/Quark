a361a1472f7f1b99955cc893025d0d3f26998bcf
Vasco Lavos <vazkii@hotmail.com>
2023-01-10 15:55:59 +0000

    Gold swords have looting instead of fortune

--- src/main/java/vazkii/quark/content/tweaks/module/GoldToolsHaveFortuneModule.java
+++ src/main/java/vazkii/quark/content/tweaks/module/GoldToolsHaveFortuneModule.java
@@ -1,12 +1,19 @@
 package vazkii.quark.content.tweaks.module;
 
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.DiggerItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.TieredItem;
 import net.minecraft.world.item.Tiers;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraftforge.event.entity.living.LootingLevelEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import vazkii.quark.base.module.LoadModule;
 import vazkii.quark.base.module.ModuleCategory;
 import vazkii.quark.base.module.QuarkModule;
@@ -14,7 +21,7 @@ import vazkii.quark.base.module.config.Config;
 import vazkii.quark.base.module.config.Config.Max;
 import vazkii.quark.base.module.config.Config.Min;
 
-@LoadModule(category = ModuleCategory.TWEAKS)
+@LoadModule(category = ModuleCategory.TWEAKS, hasSubscriptions = true)
 public class GoldToolsHaveFortuneModule extends QuarkModule {
 
 	private static final Tier[] TIERS = new Tier[] {
@@ -22,6 +29,7 @@ public class GoldToolsHaveFortuneModule extends QuarkModule {
 	};
 
 	@Config
+	@Min(0)
 	public static int fortuneLevel = 2;
 
 	@Config
@@ -35,21 +43,44 @@ public class GoldToolsHaveFortuneModule extends QuarkModule {
 	public void configChanged() {
 		staticEnabled = enabled;
 	}
-
-	public static int getFortuneLevel(Enchantment enchant, ItemStack stack, int prev) {
-		if(!staticEnabled || enchant != Enchantments.BLOCK_FORTUNE || prev >= fortuneLevel)
-			return prev;
-
-		if(stack.getItem() instanceof DiggerItem di) {
-			Tier tier = di.getTier();
+	
+	@SubscribeEvent
+	public void onLootingCheck(LootingLevelEvent event) {
+		DamageSource source = event.getDamageSource();
+		if(source != null && source.getEntity() != null) {
+			Entity entity = source.getEntity();
+			if(entity instanceof LivingEntity le) {
+				ItemStack stack = le.getMainHandItem();
+				
+				if(stack.getItem() instanceof SwordItem) {
+					int level = event.getLootingLevel();
+					int target = getEffectiveLevel(stack, level);
+					
+					if(target > level)
+						event.setLootingLevel(target);
+				}
+			}
+		}
+	}
+	
+	private static int getEffectiveLevel(ItemStack stack, int prevLvl) {
+		if(stack.getItem() instanceof TieredItem ti) {
+			Tier tier = ti.getTier();
 			
 			if(tier == Tiers.GOLD)
-				return 2;
+				return fortuneLevel;
 		}
-
-		return prev;
+		
+		return prevLvl;
 	}
 
+	public static int getFortuneLevel(Enchantment enchant, ItemStack stack, int prev) {
+		if(!staticEnabled || prev >= fortuneLevel || enchant != Enchantments.BLOCK_FORTUNE || !(stack.getItem() instanceof DiggerItem))
+			return prev;
+
+		return getEffectiveLevel(stack, prev);
+	}
+	
 	public static Tier getEffectiveTier(Item item, Tier realTier) {
 		if(!staticEnabled || (realTier != Tiers.GOLD))
 			return realTier;
