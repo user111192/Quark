a188a96535f46ef379d3994c25e34ebf356b3373
Vasco Lavos <vazkii@hotmail.com>
2023-01-16 18:48:00 +0000

    Safe Rabbits is now Safer Creatures, with slime support

--- src/main/java/vazkii/quark/content/tweaks/module/SaferCreaturesModule.java
+++ src/main/java/vazkii/quark/content/tweaks/module/SaferCreaturesModule.java
@@ -1,6 +1,8 @@
 package vazkii.quark.content.tweaks.module;
 
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.entity.living.LivingFallEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import vazkii.quark.base.module.LoadModule;
@@ -9,15 +11,31 @@ import vazkii.quark.base.module.QuarkModule;
 import vazkii.quark.base.module.config.Config;
 
 @LoadModule(category = ModuleCategory.TWEAKS, hasSubscriptions = true)
-public class SafeRabbitsModule extends QuarkModule {
+public class SaferCreaturesModule extends QuarkModule {
 
 	@Config(description = "How many blocks should be subtracted from the rabbit fall height when calculating fall damage. 5 is the same value as vanilla frogs") 
 	public double heightReduction = 5.0;
 	
+	@Config
+	public boolean enableSlimeFallDamageRemoval = true;
+	
 	@SubscribeEvent
 	public void onFall(LivingFallEvent event) {
-		if(event.getEntity().getType() == EntityType.RABBIT)
-			event.setDistance(Math.max(0, event.getDistance() - (float) heightReduction));
+		Entity e = event.getEntity();
+		EntityType<?> type = e.getType();
+		float dist = event.getDistance();
+		
+		if(type == EntityType.RABBIT)
+			event.setDistance(Math.max(0, dist - (float) heightReduction));
+		
+		else if(type == EntityType.SLIME && enableSlimeFallDamageRemoval) {
+			if(dist > 2) {
+				Vec3 movement = e.getDeltaMovement();
+				e.setDeltaMovement(movement.x, -2, movement.z);
+			}
+			
+			event.setDistance(0);
+		}
 	}
 	
 }
